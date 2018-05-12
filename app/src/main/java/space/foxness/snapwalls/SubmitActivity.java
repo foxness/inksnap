2c23781bc7621c81a25da82076d9435acd3d8afd
Nocturnal <foxnezz@gmail.com>
2018-05-12 14:15:58 +0300

    added the submit button & changed the language level to 8

--- app/src/main/java/space/foxness/snapwalls/SubmitActivity.java
+++ app/src/main/java/space/foxness/snapwalls/SubmitActivity.java
@@ -2,14 +2,22 @@ package space.foxness.snapwalls;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SubmitActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        Button submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(v ->
+        {
+            Toast.makeText(this, "Testy is besty!", Toast.LENGTH_LONG).show();
+        });
     }
 }
