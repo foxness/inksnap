6344965995c6939216f5759441d5814a655ce290
Nocturnal <foxnezz@gmail.com>
2018-05-21 21:59:47 +0300

    made singletons thread-safe

--- app/src/main/java/space/foxness/snapwalls/Queue.kt
+++ app/src/main/java/space/foxness/snapwalls/Queue.kt
@@ -9,33 +9,28 @@ class Queue private constructor(context: Context) {
 
     private val dbDao: PostDao
 
-    val posts: List<Post>
-        get() = dbDao.posts
-
-    init {
-        dbDao = Room.databaseBuilder<AppDatabase>(context.applicationContext, AppDatabase::class.java, databaseName).allowMainThreadQueries().build().postDao()
-        // allowMainThreadQueries() is a dirty hack
-        // todo: fix this by going async
-    }
+    val posts: List<Post> get() = dbDao.posts
 
     fun addPost(post: Post) = dbDao.addPost(post)
 
     fun getPost(id: Long) = dbDao.getPostById(id)
-    
+
     fun updatePost(post: Post) = dbDao.updatePost(post)
-    
+
     fun deletePost(id: Long) = dbDao.deletePostbyId(id)
+    
+    init {
+        dbDao = Room
+                .databaseBuilder<AppDatabase>(context.applicationContext, AppDatabase::class.java, databaseName)
+                .allowMainThreadQueries()
+                .build()
+                .postDao()
+        
+        // allowMainThreadQueries() is a dirty hack
+        // todo: fix this by going async
+    }
 
-    companion object {
+    companion object : SingletonHolder<Queue, Context>(::Queue) {
         private const val databaseName = "queue"
-
-        private var queueInstance: Queue? = null
-
-        fun getInstance(context: Context): Queue {
-            if (queueInstance == null)
-                queueInstance = Queue(context)
-
-            return queueInstance as Queue
-        }
     }
 }
