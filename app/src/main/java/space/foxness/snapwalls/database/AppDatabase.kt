b502b274099f779437861218764cb94a59d9000e
Nocturnal <foxnezz@gmail.com>
2018-05-22 16:45:50 +0300

    removed unused database type converters

--- app/src/main/java/space/foxness/snapwalls/database/AppDatabase.kt
+++ app/src/main/java/space/foxness/snapwalls/database/AppDatabase.kt
@@ -3,15 +3,12 @@ package space.foxness.snapwalls.database
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
-import android.arch.persistence.room.TypeConverters
 import android.content.Context
-
 import space.foxness.snapwalls.Post
 import space.foxness.snapwalls.SingletonHolder
 import space.foxness.snapwalls.database.AppDatabase.Companion.databaseName
 
 @Database(entities = [(Post::class)], version = 1)
-@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun postDao(): PostDao
     
