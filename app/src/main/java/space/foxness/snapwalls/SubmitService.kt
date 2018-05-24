91d87d625c6fb2515be2c2a110dce47f7c8f470e
Nocturnal <foxnezz@gmail.com>
2018-05-24 16:41:41 +0300

    improved submission error logging

--- app/src/main/java/space/foxness/snapwalls/SubmitService.kt
+++ app/src/main/java/space/foxness/snapwalls/SubmitService.kt
@@ -11,17 +11,16 @@ import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 import space.foxness.snapwalls.Util.log
 
-
 class SubmitService : Service() {
     
     private lateinit var mServiceLooper: Looper
     private lateinit var mServiceHandler: ServiceHandler
-
+    
     private inner class ServiceHandler(looper: Looper) : Handler(looper) {
 
         override fun handleMessage(msg: Message) {
 
-            log("I AM SUBMIT")
+            log("I am trying to submit...")
 
             // todo: add all posts that failed to be submitted to a 'failed' list
             // todo: add network safeguard to auth
@@ -29,42 +28,36 @@ class SubmitService : Service() {
             // BIG NOTE: stopSelf(msg.arg1) MUST BE CALLED BEFORE RETURNING
             // DON'T RETURN WITHOUT CALLING IT
 
-            // this code is impossible to format to look good with this brace styling :(
-            
             val intent = msg.obj as Intent
             val postId = intent.getLongExtra(EXTRA_POST_ID, -1)
             val post = Queue.getInstance(this@SubmitService).getPost(postId)
-            if (post == null) {
+            val goodPost = post != null
 
-                log("POST NOT FOUND")
-
-            } else {
-
-                val reddit = Autoreddit.getInstance(this@SubmitService).reddit
-                if (!reddit.canSubmitRightNow) {
-
-                    log("NOT LOGGED IN OR RATELIMITED")
-
-                } else {
-
-                    if (!isNetworkAvailable()) {
-
-                        log("NETWORK NOT AVAILABLE")
-
-                    } else {
-
-                        if (!DEBUG_DONT_POST) {
-                            reddit.submit(post, { error, link ->
-                                if (error != null) {
-                                    log("ERROE: ${error.message}")
-                                    throw error
-                                }
-
-                                log("GOT LINK: $link")
-                            })
-                        }
+            val reddit =  Autoreddit.getInstance(this@SubmitService).reddit
+            val signedIn = reddit.isSignedIn
+            val notRatelimited = !reddit.isRestrictedByRatelimit
+            
+            val networkAvailable = isNetworkAvailable()
+            val notDebugging = !DEBUG_DONT_POST
+            
+            val everythingGood = goodPost 
+                    && signedIn 
+                    && notRatelimited 
+                    && networkAvailable 
+                    && notDebugging
+            
+            // todo: make reddit.submit() async !!
+            if (everythingGood) {
+                reddit.submit(post!!, { error, link ->
+                    if (error != null) {
+                        log("ERROR DURING SUBMISSION: ${error.message}")
+                        throw error
                     }
-                }
+
+                    log("Successfully submitted a post. Link: $link")
+                })
+            } else {
+                log(constructErrorMessage(post, goodPost, signedIn, notRatelimited, networkAvailable, notDebugging))
             }
 
             stopSelf(msg.arg1)
@@ -97,6 +90,8 @@ class SubmitService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
+        // todo: remove the possibility of 2 parallel submissions
+        
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
             createNotificationChannel()
         
@@ -120,7 +115,7 @@ class SubmitService : Service() {
     override fun onBind(intent: Intent): IBinder? = null
 
     companion object {
-        private const val DEBUG_DONT_POST = true
+        private const val DEBUG_DONT_POST = false
         private const val NOTIFICATION_ID = 1 // must not be 0
         private const val NOTIFICATION_CHANNEL_NAME = "Main"
         private const val NOTIFICATION_CHANNEL_ID = NOTIFICATION_CHANNEL_NAME
@@ -130,6 +125,45 @@ class SubmitService : Service() {
             val i = Intent(context, SubmitService::class.java)
             i.putExtra(EXTRA_POST_ID, postId)
             return i
+        }
+        
+        // assumes that not all of the arguments are true
+        private fun constructErrorMessage(post: Post?,
+                                          goodPost: Boolean,
+                                          signedIn: Boolean,
+                                          notRatelimited: Boolean,
+                                          networkAvailable: Boolean,
+                                          notDebugging: Boolean): String {
+
+            val reasonsDidntPost = mutableListOf<String>()
+
+            val postNotFoundReason = "didn't find the post in the database"
+            val notSignedInReason = "wasn't signed in"
+            val ratelimitedReason = "was ratelimited"
+            val networkNotAvailableReason = "network wasn't available"
+            val debuggingReason = "was debugging"
+
+            if (!goodPost) reasonsDidntPost.add(postNotFoundReason)
+            if (!signedIn) reasonsDidntPost.add(notSignedInReason)
+            if (!notRatelimited) reasonsDidntPost.add(ratelimitedReason)
+            if (!networkAvailable) reasonsDidntPost.add(networkNotAvailableReason)
+            if (!notDebugging) reasonsDidntPost.add(debuggingReason)
+
+            val postString = if (goodPost)
+                "the post titled '${post!!.title}' with ID ${post.id}"
+            else
+                "this post"
+
+            var errorMessage = "Couldn't submit $postString because "
+
+            reasonsDidntPost.forEachIndexed { index, reason ->
+                errorMessage += if (index == reasonsDidntPost.size - 1)
+                    reason
+                else
+                    "$reason and "
+            }
+            
+            return errorMessage
         }
     }
 }
