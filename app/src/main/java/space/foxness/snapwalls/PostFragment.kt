2afc527612001d5283d6679bff9e2c9690c00242
Nocturnal <foxnezz@gmail.com>
2018-05-25 22:13:26 +0300

    added post validation

--- app/src/main/java/space/foxness/snapwalls/PostFragment.kt
+++ app/src/main/java/space/foxness/snapwalls/PostFragment.kt
@@ -4,18 +4,22 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.*
+import android.webkit.URLUtil.isValidUrl
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Switch
+import space.foxness.snapwalls.Util.toast
 
 class PostFragment : Fragment() {
+    
+    private lateinit var titleEdit: EditText
+    private lateinit var contentEdit: EditText
+    private lateinit var subredditEdit: EditText
+    private lateinit var typeSwitch: Switch
 
     private lateinit var queue: Queue
     private lateinit var post: Post
-    private var isValidPost = false // todo: don't let user create invalid posts
     private var newPost = false
 
     // todo: account for submitservice
@@ -57,7 +61,7 @@ class PostFragment : Fragment() {
         }
     }
 
-    private fun deletePost() { // todo: clear activity result if newpost
+    private fun deletePost() {
         if (newPost) {
             clearResult()
         } else {
@@ -72,67 +76,59 @@ class PostFragment : Fragment() {
 
         // TITLE EDIT -------------------------
         
-        val titleEdit = v.findViewById<EditText>(R.id.post_title)
+        titleEdit = v.findViewById(R.id.post_title)
         titleEdit.setText(post.title)
-        titleEdit.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                post.title = s.toString()
-                updateIsValidPost()
-            }
-
-            override fun afterTextChanged(s: Editable) { }
-        })
         
         // CONTENT EDIT -----------------------
 
-        val contentEdit = v.findViewById<EditText>(R.id.post_content)
+        contentEdit = v.findViewById(R.id.post_content)
         contentEdit.setText(post.content)
-        contentEdit.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                post.content = s.toString()
-                updateIsValidPost()
-            }
-
-            override fun afterTextChanged(s: Editable) { }
-        })
         
         // SUBREDDIT EDIT ---------------------
 
-        val subredditEdit = v.findViewById<EditText>(R.id.post_subreddit)
+        subredditEdit = v.findViewById(R.id.post_subreddit)
         subredditEdit.setText(post.subreddit)
-        subredditEdit.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                post.subreddit = s.toString()
-                updateIsValidPost()
-            }
-
-            override fun afterTextChanged(s: Editable) { }
-        })
         
         // TYPE SWITCH ------------------------
 
-        val typeSwitch = v.findViewById<Switch>(R.id.post_type)
+        typeSwitch = v.findViewById(R.id.post_type)
         typeSwitch.isChecked = post.type
-        typeSwitch.setOnCheckedChangeListener { _, isChecked -> post.type = isChecked }
         
         // SAVE BUTTON ------------------------
         
         val saveButton = v.findViewById<Button>(R.id.save_button)
-        saveButton.setOnClickListener {  // todo: refuse to save an invalid post
-            if (newPost) {
-                setNewPostResult()
+        saveButton.setOnClickListener {
+            
+            unloadViewsToPost()
+
+            val notEmptyTitle = !post.title.isEmpty()
+            val notEmptySubreddit = !post.subreddit.isEmpty()
+            val validContent = !(post.type && !isValidUrl(post.content))
+
+            val isPostValid = notEmptyTitle && validContent && notEmptySubreddit
+            
+            if (isPostValid) {
+                
+                if (newPost)
+                    setNewPostResult()
+                else
+                    queue.updatePost(post)
+                
+                activity!!.finish()
+                
             } else {
-                queue.updatePost(post)
+                toast(constructDenyMessage(notEmptyTitle, post.content, notEmptySubreddit, post.type))
             }
         }
 
         return v
+    }
+    
+    private fun unloadViewsToPost() {
+        post.title = titleEdit.text.toString()
+        post.content = contentEdit.text.toString()
+        post.subreddit = subredditEdit.text.toString()
+        post.type = typeSwitch.isChecked
     }
     
     private fun setNewPostResult() {
@@ -145,18 +141,26 @@ class PostFragment : Fragment() {
         activity!!.setResult(Activity.RESULT_CANCELED)
     }
 
-    private fun updateIsValidPost() {
-        val validTitle = !post.title.isEmpty()
-        val validContent = !(post.type && post.content.isEmpty()) // todo: add url validation
-        val validSubreddit = !post.subreddit.isEmpty()
-
-        isValidPost = validTitle && validContent && validSubreddit
-    }
-
     companion object {
         private const val ARG_POST_ID = "post_id"
         private const val ARG_NEW_POST = "new_post"
         private const val RESULT_NEW_POST = "new_post"
+        
+        private fun constructDenyMessage(notEmptyTitle: Boolean,
+                                         content: String,
+                                         notEmptySubreddit: Boolean,
+                                         type: Boolean): String {
+            if (!notEmptyTitle)
+                return "Title must not be empty"
+            
+            if (!notEmptySubreddit)
+                return "Subreddit must not be empty"
+            
+            if (type && !isValidUrl(content))
+                return "Url must be valid"
+            
+            throw Exception("Bad logic somewhere in SaveButton.Click") // this should never happen
+        }
         
         fun getNewPostFromResult(data: Intent)
                 = data.getSerializableExtra(RESULT_NEW_POST) as? Post
