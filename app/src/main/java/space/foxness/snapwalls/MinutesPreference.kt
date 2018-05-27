1e9eb801f4213d0c75ee58e9e15667cf6acac0b2
Nocturnal <foxnezz@gmail.com>
2018-05-27 22:05:23 +0300

    added minutes picker

---
+++ app/src/main/java/space/foxness/snapwalls/MinutesPreference.kt
@@ -0,0 +1,32 @@
+package space.foxness.snapwalls
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.support.v7.preference.DialogPreference
+import android.util.AttributeSet
+
+class MinutesPreference(context: Context, attrs: AttributeSet?) : DialogPreference(context, attrs) {
+    
+    private var _minutes = 0
+    
+    var minutes
+        get() = _minutes
+        set(value) {
+            _minutes = value
+            persistInt(value)
+        }
+
+    override fun getDialogLayoutResource() = R.layout.dialog_minutespicker
+    
+    init {
+        setPositiveButtonText(android.R.string.ok)
+        setNegativeButtonText(android.R.string.cancel)
+    }
+
+    override fun onGetDefaultValue(a: TypedArray?, index: Int)
+        = a!!.getInt(index, 0)
+
+    override fun onSetInitialValue(restorePersistedValue: Boolean, defaultValue: Any?) {
+        minutes = if (restorePersistedValue) getPersistedInt(_minutes) else defaultValue as Int
+    }
+}
--- app/src/main/java/space/foxness/snapwalls/SettingsFragment.kt
+++ app/src/main/java/space/foxness/snapwalls/SettingsFragment.kt
@@ -1,11 +1,33 @@
 package space.foxness.snapwalls
 
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        var dialogFragment: DialogFragment? = null
+        if (preference is MinutesPreference) {
+            dialogFragment = MinutesPreferenceFragmentCompat.newInstance(preference.key)
+        }
+        
+        if (dialogFragment != null) {
+            dialogFragment.setTargetFragment(this, REQUEST_CODE)
+            dialogFragment.show(fragmentManager,
+                    "android.support.v7.preference.PreferenceFragment.DIALOG")
+        }
+        else {
+            super.onDisplayPreferenceDialog(preference)
+        }
+    }
+    
+    companion object {
+        private const val REQUEST_CODE = 0
     }
 }
