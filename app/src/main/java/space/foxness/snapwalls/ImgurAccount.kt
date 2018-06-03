a19c1df6d1911c0315bbfdec0cc16bb794d39d14
Nocturnal <foxnezz@gmail.com>
2018-06-03 16:39:01 +0300

    added imgur access token refreshing

--- app/src/main/java/space/foxness/snapwalls/ImgurAccount.kt
+++ app/src/main/java/space/foxness/snapwalls/ImgurAccount.kt
@@ -1,6 +1,7 @@
 package space.foxness.snapwalls
 
 import android.net.Uri
+import android.webkit.URLUtil.isValidUrl
 import org.joda.time.DateTime
 import org.joda.time.Duration
 
@@ -62,8 +63,43 @@ class ImgurAccount(private val callbacks: Callbacks) {
         callbacks.onNewAccessToken(accessToken!!, accessTokenExpirationDate!!)
     }
     
+    private fun ensureValidAccessToken() {
+        if (accessToken != null && accessTokenExpirationDate!! > DateTime.now())
+            return
+
+        val refreshToken = refreshToken ?: throw Exception("Can't update access token without refresh token")
+
+        val headers = mapOf("User-Agent" to USER_AGENT)
+
+        val data = mapOf(
+                "grant_type" to "refresh_token",
+                "refresh_token" to refreshToken,
+                "client_id" to APP_CLIENT_ID,
+                "client_secret" to APP_CLIENT_SECRET)
+
+        val response = khttp.post(url = ACCESS_TOKEN_ENDPOINT, headers = headers, data = data)
+
+        if (response.statusCode != 200)
+            throw Exception("Response code: ${response.statusCode}, response: $response")
+
+        val json = response.jsonObject
+        
+        val newAccessToken = json.getString("access_token")
+        val expiresIn = json.getLong("expires_in")
+        
+        // idk why imgur returns a new refresh token here
+        // the old refresh token keeps working but i save the new one just in case
+        val newRefreshToken = json.getString("refresh_token")
+        
+        updateRefreshToken(newRefreshToken)
+        updateAccessToken(newAccessToken, expiresIn)
+    }
+    
     fun uploadImage(url: String): String {
-        // todo: ensureValidAccessToken()
+        if (!isValidUrl(url))
+            throw Exception("Invalid url: $url")
+        
+        ensureValidAccessToken()
         
         val headers = mapOf(
                 "User-Agent" to USER_AGENT,
@@ -88,13 +124,14 @@ class ImgurAccount(private val callbacks: Callbacks) {
     
     companion object {
         private const val APP_CLIENT_ID = "f74e1f0ca375c60"
-//        private const val APP_CLIENT_SECRET = "9d7910e7976b44b36feaafdd7e0cd3e74daad29a"
+        private const val APP_CLIENT_SECRET = "9d7910e7976b44b36feaafdd7e0cd3e74daad29a"
         private const val APP_REDIRECT_URI = "https://localhost/"
         
         private const val AUTH_RESPONSE_TYPE = "token"
 
         private const val IMAGE_UPLOAD_ENDPOINT = "https://api.imgur.com/3/image"
         private const val AUTHORIZATION_ENDPOINT = "https://api.imgur.com/oauth2/authorize"
+        private const val ACCESS_TOKEN_ENDPOINT = "https://api.imgur.com/oauth2/token"
 
         private const val USER_AGENT = "Snapwalls by /u/foxneZz" // todo: extract this to util?
         
