25f2a5b97a22161d26114b51b505c6a4fcc0228d
nocturnal <foxnezz@gmail.com>
2018-08-08 17:57:11 +0300

    added post type tabs

--- app/src/main/kotlin/space/foxness/snapwalls/NewpostFragment.kt
+++ app/src/main/kotlin/space/foxness/snapwalls/NewpostFragment.kt
@@ -1,12 +1,14 @@
 package space.foxness.snapwalls
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import space.foxness.snapwalls.Util.log
 
 class NewpostFragment : Fragment()
 {
@@ -25,7 +27,38 @@ class NewpostFragment : Fragment()
             }
 
             override fun getCount() = 2
+
+            override fun getPageTitle(position: Int): CharSequence?
+            {
+                return when (position)
+                {
+                    0 -> "self"
+                    1 -> "link"
+                    else -> throw Exception("how")
+                }
+            }
         }
+        
+        val otsl = object : TabLayout.OnTabSelectedListener
+        {
+            override fun onTabSelected(tab: TabLayout.Tab)
+            {
+                log("tab ${tab.text} at ${tab.position} selected")
+            }
+            
+            override fun onTabReselected(tab: TabLayout.Tab)
+            {
+                log("tab ${tab.text} at ${tab.position} reselected")
+            }
+            
+            override fun onTabUnselected(tab: TabLayout.Tab)
+            {
+                log("tab ${tab.text} at ${tab.position} unselected")
+            }
+        }
+        
+        val tabLayout = v.findViewById<TabLayout>(R.id.newpost_tablayout)
+        tabLayout.addOnTabSelectedListener(otsl)
         
         return v
     }
