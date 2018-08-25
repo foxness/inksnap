e514c33be478f6662f3e3dabc54d5a39500389a6
nocturnal <foxnezz@gmail.com>
2018-08-25 21:14:41 +0300

    added release signing & release variant variables

---
+++ app/src/release/kotlin/me/nocturnl/inksnap/VariantVariables.kt
@@ -0,0 +1,12 @@
+package me.nocturnl.inksnap
+
+object VariantVariables
+{
+    const val VARIANT_NAME = "release"
+    
+    const val REDDIT_CLIENT_ID = "fE1cmNxK4aTCQg"
+    const val REDDIT_CLIENT_SECRET = ""
+    
+    const val IMGUR_CLIENT_ID = "f2daa5bfa0d874b"
+    const val IMGUR_CLIENT_SECRET = "acd0556985b67837d728f4bba02c38daa3134e4f"
+}
